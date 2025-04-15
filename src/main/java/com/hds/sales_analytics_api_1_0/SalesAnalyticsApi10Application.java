833564fd86a9ae6c5324821097ba3ca078ef9362
Nishanth Yenepalli <nishanth.yenepalli@hitachids.com>
2025-04-15 13:55:54 +0530

    login feature is completed

--- src/main/java/com/hds/sales_analytics_api_1_0/SalesAnalyticsApi10Application.java
+++ src/main/java/com/hds/sales_analytics_api_1_0/SalesAnalyticsApi10Application.java
@@ -2,12 +2,18 @@ package com.hds.sales_analytics_api_10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SalesAnalyticsApi10Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalesAnalyticsApi10Application.class, args);
+	}
+	
+	@GetMapping("/hi")
+	public String user() {
+		return "Hello";
 	}
 
 }
